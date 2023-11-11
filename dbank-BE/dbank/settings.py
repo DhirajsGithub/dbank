7b40b1087788ec9d3c60ac87a3c06b9e3dfb4b59
DhirajsGithub <borsedhiraj123@gmail.com>
2023-11-12 01:05:48 +0530

    ss

--- dbank-BE/dbank/settings.py
+++ dbank-BE/dbank/settings.py
@@ -53,16 +53,23 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 
+
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware'
-]
+ 'corsheaders.middleware.CorsMiddleware',
+ 'django.middleware.security.SecurityMiddleware',
+
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+
+ 'django.contrib.sessions.middleware.SessionMiddleware',
+ 'django.middleware.common.CommonMiddleware',
+ 'django.middleware.csrf.CsrfViewMiddleware',
+ 'django.contrib.auth.middleware.AuthenticationMiddleware',
+ 'django.contrib.messages.middleware.MessageMiddleware',
+
+
+ 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+  ]
 
 
 ROOT_URLCONF = "dbank.urls"
